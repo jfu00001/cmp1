34ac4294acc91e37c8969db39c6a9b766c9193ef
Jiayi Fu <fujiayi123@hotmail.com>
2024-03-02 22:11:57 +0200

    add localization

--- src/i18n.js
+++ src/i18n.js
@@ -1,5 +1,5 @@
 import i18n from "i18next";
-import { useTranslation, initReactI18next } from "react-i18next";
+import { initReactI18next } from "react-i18next";
 import enJSON from "./i18n/en.json";
 
 i18n.use(initReactI18next).init({
