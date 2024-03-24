2e3a8d211d03cf08b2e1dd19b8f2d0a45b87b305
Jiayi Fu <fujiayi123@hotmail.com>
2024-03-24 14:53:25 +0200

    homepage service section

--- src/common/servicesection/servicesection.js
+++ src/common/servicesection/servicesection.js
@@ -2,193 +2,129 @@
 import { css } from "@emotion/react";
 import { Link } from "react-router-dom";
 
-const ServiceItem = ({ src, alt, text }) => (
+const BorderedText = ({ children }) => (
   <div
     css={css`
       display: flex;
-      flex-direction: column;
-      width: 25%;
+      font-family: Cera Pro, sans-serif;
+      justify-content: center;
+      border-radius: var(--radius-lg-max-circle, 100px);
+      border: 1px solid rgba(1, 10, 13, 1);
+      padding: 8px 24px;
+
       @media (max-width: 991px) {
-        width: 100%;
-        margin-left: 0;
+        padding: 0 20px;
       }
     `}
   >
-    <div
-      css={css`
-        text-align: center;
-        justify-content: center;
-        display: flex;
-        flex-direction: column;
-        align-self: stretch;
-        font-size: 15px;
-        @media (max-width: 991px) {
-          margin-top: 40px;
-          white-space: initial;
-        }
-      `}
-    >
-      <img
-        loading="lazy"
-        src={src}
-        alt={alt}
-        css={css`
-          aspect-ratio: 1;
-          object-fit: auto;
-          object-position: center;
-          width: 75px;
-          align-self: center;
-        `}
-      />
-      <div
-        css={css`
-          font-family: "Cera Pro", sans-serif;
-          margin-top: 12px;
-        `}
-      >
-        {text}
-      </div>
-    </div>
+    {children}
   </div>
 );
 
-function PoolServices(props) {
+function ServiceSection(props) {
   return (
     <section
       css={css`
-        justify-content: center;
-        background-color: rgba(81, 173, 201, 0.1);
         display: flex;
-        padding: 50px 60px;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 54px 32px;
+        background-color: rgba(81, 173, 201, 0.1);
+        text-align: center;
+
         @media (max-width: 991px) {
-          padding: 20px 20px;
+          padding: 0 20px;
         }
       `}
     >
-      <div
+      <h1
         css={css`
-          width: 100%;
+          color: var(--Shades-blacks-black, #010a0d);
+          font: 300 34px/129% Cera Pro, sans-serif;
+
           @media (max-width: 991px) {
             max-width: 100%;
           }
         `}
       >
+        {props.title}
+      </h1>
+      <div
+        css={css`
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
+          margin-top: 24px;
+          padding: 0 60px;
+          font-size: 18px;
+          @media (max-width: 991px) {
+            padding: 0 20px;
+          }
+        `}
+      >
         <div
           css={css`
-            justify-content: center;
-            display: ${props.serviceItemsDisplay};
+            display: flex;
+            flex-direction: column;
+          `}
+        >
+          <div
+            css={css`
+              display: grid;
+              grid-template-columns: 1fr 1fr 1fr;
+              gap: 50px;
+              color: var(--Shades-blacks-black, #010a0d);
+
+              @media (max-width: 991px) {
+                flex-wrap: wrap;
+              }
+            `}
+          >
+            {props.serviceItems.slice(0, 3).map((text, index) => (
+              <BorderedText key={index}>{text}</BorderedText>
+            ))}
+          </div>
+          <div
+            css={css`
+              display: grid;
+              grid-template-columns: 1fr 1fr 1fr;
+              gap: 50px;
+              margin-top: 12px;
+              color: var(--Shades-blacks-darker, #1a2426);
+
+              @media (max-width: 991px) {
+                flex-wrap: wrap;
+              }
+            `}
+          >
+            {props.serviceItems.slice(3).map((text, index) => (
+              <BorderedText key={index}>{text}</BorderedText>
+            ))}
+          </div>
+        </div>
+      </div>
+      <Link to="/services">
+        <button
+          css={css`
+            margin-top: 24px;
+            padding: 8px 24px;
+            border-radius: var(--radius-lg-max-circle, 100px);
+            background-color: var(--Brand-colors-deep_blue, #0c85a7);
+            color: var(--color-typography-on-color---white, #fff);
+            font: 16px/150% Cera Pro, sans-serif;
+
             @media (max-width: 991px) {
-              max-width: 100%;
               padding: 0 20px;
             }
           `}
         >
-          <div
-            css={css`
-              justify-content: center;
-              display: flex;
-              gap: 20px;
-              display: flex;
-              @media (max-width: 991px) {
-                flex-direction: column;
-                align-items: stretch;
-                gap: 0px;
-              }
-            `}
-          >
-            {props.serviceItems.map((item) => (
-              <ServiceItem src={item.src} alt={item.alt} text={item.text} />
-            ))}
-          </div>
-        </div>
-        <div
-          css={css`
-            margin-top: 24px;
-            display: flex;
-            padding: 0 10vw;
-            @media (max-width: 991px) {
-              max-width: 100%;
-              display: block;
-              padding: 0 0;
-            }
-          `}
-        >
-          <div
-            css={css`
-            color: var(--Shades-blacks-black, #010a0d);
-            font: 300 34px/44px sans-serif;
-            text-align: right;
-            width: 17vw;
-            @media (max-width: 991px) {
-              margin-top: 24px;
-              width: 100%;
-              text-align: center;
-              font-size: 22px;
-              line-height: inherit;
-            }  
-            }
-          `}
-          >
-            {props.title}
-          </div>
-          <div
-            css={css`
-              width: 60vw;
-              display: flex;
-              flex-direction: column;
-              font-size: 17px;
-              color: var(--Shades-blacks-black, #010a0d);
-              font-weight: 300;
-              margin-left: 30px;
-              @media (max-width: 991px) {
-                width: 100%;
-                margin: 32px 0 0 0;
-                display: block;
-                text-align: center;
-              }
-            `}
-          >
-            <div
-              css={css`
-                font-family: "Inter", sans-serif;
-              `}
-            >
-              {props.p1}
-            </div>
-            <div
-              css={css`
-                font-family: "Inter", sans-serif;
-                margin-top: 16px;
-              `}
-            >
-              {props.p2}
-            </div>
-          </div>
-        </div>
-        {props.hasOwnProperty("ourService") ? (
-          <Link to="/services">
-            <div
-              css={css`
-                width: fit-content;
-                border-radius: var(--radius-lg-max-circle, 100px);
-                background-color: var(--Brand-colors-deep_blue, #0c85a7);
-                margin-top: 35px;
-                color: var(--color-typography-on-color---white, #fff);
-                padding: 8px 24px;
-                font: 400 16px/150% "Cera Pro", sans-serif;
-                margin-left: auto;
-                margin-right: auto;
-                @media (max-width: 991px) {
-                  margin-bottom: 30px;
-                }
-              `}
-            >
-              {props.ourService}
-            </div>
-          </Link>
-        ) : null}
-      </div>
+          {props.ourService}
+        </button>
+      </Link>
     </section>
   );
 }
-export default PoolServices;
+
+export default ServiceSection;
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,10 +4,6 @@ import CarouselWithText from "../../common/carouselwithtext/carouselwithtext";
 import { useTranslation } from "react-i18next";
 import PoolDesignInfo from "../../common/pooldesigninfo/pooldesigninfo";
 import ServiceSection from "../../common/servicesection/servicesection";
-import PoolIcon from "./servicesection/new-pool-icon.png";
-import SpaIcon from "./servicesection/spa-icon.png";
-import RenovationIcon from "./servicesection/remodel-icon.png";
-import MaintenanceIcon from "./servicesection/maintenance-icon.png";
 
 function Home() {
   const carouselImages = require.context("./carouselimages", true);
@@ -16,28 +12,10 @@ function Home() {
     .map((image) => carouselImages(image));
   const { t } = useTranslation();
 
-  const serviceItems = [
-    {
-      src: PoolIcon,
-      alt: t("services.poolService.customPools"),
-      text: t("services.poolService.customPools"),
-    },
-    {
-      src: SpaIcon,
-      alt: t("services.poolService.customSpas"),
-      text: t("services.poolService.customSpas"),
-    },
-    {
-      src: RenovationIcon,
-      alt: t("services.poolService.renovations"),
-      text: t("services.poolService.renovations"),
-    },
-    {
-      src: MaintenanceIcon,
-      alt: t("services.poolService.cleanAndMaintenance"),
-      text: t("services.poolService.cleanAndMaintenance"),
-    },
-  ];
+  var jsonData = require("../../i18n/en.json");
+  const serviceItems = jsonData.translation.list_of_services.map((service) => {
+    return service.title;
+  });
 
   return (
     <>
@@ -66,8 +44,6 @@ function Home() {
         serviceItemsDisplay="flex"
         serviceItems={serviceItems}
         title={t("services.poolService.title")}
-        p1={t("services.poolService.p1")}
-        p2={t("services.poolService.p2")}
         ourService={t("services.poolService.ourService")}
       />
     </>
